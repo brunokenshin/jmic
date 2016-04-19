1d7217e615705be2b515f5f12d22d16f13591463
Urso <brunoodin@gmail.com>
2016-04-18 21:00:25 -0300

    Creates project structure

---
+++ jmic/src/main/java/br/com/ursos/JmicApplication.java
@@ -0,0 +1,12 @@
+package br.com.ursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmicApplication.class, args);
+	}
+}
