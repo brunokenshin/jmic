1088f12e92f7395934e711fd82be28f8d6918fee
Urso <brunoodin@gmail.com>
2016-06-03 22:33:40 -0300

    Sets content-type for response

--- jmic/src/main/java/br/com/ursos/controller/MailController.java
+++ jmic/src/main/java/br/com/ursos/controller/MailController.java
@@ -1,5 +1,7 @@
 package br.com.ursos.controller;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -18,19 +20,21 @@ public class MailController {
 
 	private final MailService mailService;
 
+	private final String contentType = "application/json;charset=UTF-8";
+
 	@Autowired
 	public MailController(MailService mailService) {
 		this.mailService = mailService;
 	}
 
 	@ResponseBody
-	@RequestMapping("/report")
+	@RequestMapping(value = "/report", method = GET, produces = contentType)
 	public FieldList reportFields() throws MessagingException, IOException, SQLException {
 		return mailService.reportFields();
 	}
 
 	@ResponseBody
-	@RequestMapping("/import")
+	@RequestMapping(value = "/import", method = GET, produces = contentType)
 	public FieldList importFields() throws MessagingException, IOException, SQLException {
 		return mailService.persistFields();
 	}
